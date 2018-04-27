67dca4d85802caedd816573f625955b8d7d0639c
Rodrigo Teruel <rodrigo.teruel@hotmail.com>
2018-04-27 14:10:49 -0300

    atualizações e tela 3

---
+++ src/app/modelo.ts
@@ -0,0 +1,11 @@
+export class Modelo {
+
+    constructor(
+        public _id:number = Math.floor(Math.random()*100),
+        public questionum: number = 0,
+        public questiondois: number = 0,
+        public questiontres: number = 0,
+        public questionquatro: number = 0
+    ){}
+}
+
--- src/app/tela-tres/tela-tres.component.ts
+++ src/app/tela-tres/tela-tres.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Modelo } from './../modelo';
+import { Component, OnInit, Input } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+
+
+
 
 @Component({
   selector: 'app-tela-tres',
@@ -8,8 +13,12 @@ import 'rxjs/add/operator/map';
   styleUrls: ['./tela-tres.component.css']
 })
 export class TelaTresComponent implements OnInit {
+  modelo: Modelo = new Modelo();
+  submitted: boolean = false; //check if the form is submitted
+
   private apiUrl = 'https://viacep.com.br/ws/MG/Uberaba/barao/json/';
   data: any = {};
+ 
 
   constructor(private http: Http){
     console.log('GET data!');
@@ -29,7 +38,40 @@ export class TelaTresComponent implements OnInit {
     })
   }
  
-  ngOnInit() {
+ 
+
+  create(){
+    //call server to save
+    console.log("entrou no create");
+    this.submitted = true;
+    
   }
+
+  primeira(valor: any){
+    this.modelo.questionum = this.data[valor].cep;
+  }
+
+  segunda(valor: any){
+    this.modelo.questiondois = this.data[valor].cep;
+  }
+
+  terceira(valor: any){
+    this.modelo.questiontres = this.data[valor].cep;
+  }
+
+  quarta(){
+
+  }
+
+
+  ngOnInit() {
+     
+  }
+
+ 
+
+ 
+
+ 
 
 }
